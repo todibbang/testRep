fd504decb0eba7ca8e3eb8de06194c385068b2e4
todibbang <todibbang@gmail.com>
2017-04-18 13:38:02 +0200

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Tobias Bang on 18/04/2017.
+//  Copyright © 2017 Tobias Bang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
